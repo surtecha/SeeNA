a69f0864ceb805d78f647a15daa7a9d0330004d0
surtecha <surtecha@gmail.com>
2026-08-29 15:03:39 +1000

    Initial Commit

---
+++ SeeNA/SeeNAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeNAApp.swift
+//  SeeNA
+//
+//  Created by Suryateja Challa on 29/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct SeeNAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
